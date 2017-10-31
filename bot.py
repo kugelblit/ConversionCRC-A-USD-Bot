0fb2a2d3dec0b29c4e06bed2ba0eaf739a16de2a
David Azofeifa <david.azoherra@gmail.com>
2017-10-31 11:40:46 -0600

    Inicial Commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+﻿from telegram import *
+from telegram.ext import *
+import logging
+import requests
+
+class bot:
+    def __init__(self, token, api_key):
+        self.token = token
+        self.apikey = api_key
+        self.updater = Updater(token=self.token)
+
+        self.dispatcher = self.updater.dispatcher
+
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+        start_handler = CommandHandler(command="start", callback=self.give_exchange)
+        self.dispatcher.add_handler(start_handler)
+
+        give_handler = CommandHandler(command="give", callback=self.give_exchange)
+        self.dispatcher.add_handler(give_handler)
+
+        self.updater.start_polling()
+
+    def give_exchange(self, bot, update):
+        try:
+            exchange = requests.get(self.api_key)
+
+            exchange_rate = exchange.json()["quotes"]["USDCRC"]
+
+            update.message.reply_text(text=f"Compra dólar:  {round(exchange_rate, 2)} colones")
+
+        except:
+            update.message.reply_text(text="Algo salio mal, intente de nuevo.")
